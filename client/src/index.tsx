b0c33ba3ec4b367917ec27a1fb51aa9fe6270418
Khiro Sasha <sasha.khiro@gmail.com>
2023-03-04 23:44:07 +0200

    created store for app and implement

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Store from './store/store';
+
+interface State{
+  store: Store,
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = new Store();
+
+export const Context = createContext<State>({
+  store,
+})
+
 root.render(
-  <React.StrictMode>
+  <Context.Provider value={{
+    store
+  }}>
     <App />
-  </React.StrictMode>
+  </Context.Provider>
 );
 
